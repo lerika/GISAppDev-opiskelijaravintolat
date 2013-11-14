56f885ff1b719db9f0e2c5ddf453330dea5062d6
JuhoPajukoski <juho.pajukoski@aalto.fi>
2013-11-14 17:34:43 +0200

    Lisää ravintoloita

--- Opiskelijaravintolat/protected/data/create_script.sql
+++ Opiskelijaravintolat/protected/data/create_script.sql
@@ -8,7 +8,8 @@ CREATE TABLE ravintolat
   nimi character varying(255) NOT NULL,
   xkoord numeric(8,6),
   ykoord numeric(8,6),
-  osoite character varying(255) NOT NULL
+  osoite character varying(255) NOT NULL,
+  kunta character varying(255) NOT NULL
 )
 WITH (
   OIDS=FALSE
@@ -17,6 +18,21 @@ ALTER TABLE ravintolat
   OWNER TO postgres;
   
 INSERT INTO ravintolat(
-            nimi, xkoord, ykoord, osoite)
-    VALUES ('Dipoli', 24.831501, 60.184785, 'Otakaari 24');
+            nimi, xkoord, ykoord, osoite, kunta)
+    VALUES ('Dipoli', 24.831501, 60.184785, 'Otakaari 24', 'Espoo');
+		
+INSERT INTO ravintolat(
+            nimi, xkoord, ykoord, osoite, kunta)
+	VALUES ('Alvari', 24.82725, 60.185849, 'Otakaari 1', 'Espoo');	
 
+INSERT INTO ravintolat(
+            nimi, xkoord, ykoord, osoite, kunta)
+	VALUES ('Kvarkki', 24.829733, 60.188289, 'Otakaari 3', 'Espoo');	
+	
+INSERT INTO ravintolat(
+            nimi, xkoord, ykoord, osoite, kunta)
+	VALUES ('TUAS-talo', 24.819358, 60.186881, 'Otaniementie 17', 'Espoo');
+	
+INSERT INTO ravintolat(
+            nimi, xkoord, ykoord, osoite, kunta)
+	VALUES ('Taffa', 24.832387, 60.185926, 'Otakaari 22', 'Espoo');
