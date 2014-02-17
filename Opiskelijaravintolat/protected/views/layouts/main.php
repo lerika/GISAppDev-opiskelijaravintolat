473aa37d48c64a18f140775b25988ba242cb8987
lerika <erika.salmivaara@hotmail.com>
2014-02-17 23:30:07 +0200

    minor fixes

--- Opiskelijaravintolat/protected/views/layouts/main.php
+++ Opiskelijaravintolat/protected/views/layouts/main.php
@@ -1,8 +1,8 @@
 <?php /* @var $this Controller */ ?>
+<!DOCTYPE HTML>
 <html>
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-	<meta name="language" content="en" />
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0"/>
     <?php Yii::app()->clientScript->registerCoreScript('jquery'); ?>
     <?php Yii::app()->clientScript->registerScriptFile(Yii::app()->baseUrl.'/scripts/map.js',CClientScript::POS_END); ?>
